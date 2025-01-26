5acbc75949d14e21d99e9fa7337f29065b0bec5e
Nikita Smirnov <nvsmrnv@gmail.com>
2025-01-27 00:22:12 +0300

    add decorator for user check
    -Вынес проверку пользователя в декоратор.

--- bot/bot.py
+++ bot/bot.py
@@ -15,13 +15,21 @@ TELEGRAM_BOT_TOKEN = config('TELEGRAM_BOT_TOKEN', cast=str)
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 
+def access_required(handler):
+    def wrapper(message: telebot.types.Message, *args, **kwargs):
+        if not is_user_allowed(message.from_user.id):
+            bot.send_message(
+                chat_id=message.chat.id,
+                text='У Вас нет доступа к функционалу этого бота.'
+            )
+            return
+        return handler(message, *args, **kwargs)
+    return wrapper
+
+
 @bot.message_handler(commands=['start'])
+@access_required
 def send_welcome(message: telebot.types.Message) -> None:
-    uid = message.from_user.id
-    if not is_user_allowed(uid):
-        bot.send_message(message.chat.id, 'У Вас нет доступа к фунционалу этого бота.')
-        return 
-    
     bot.send_message(
         message.chat.id,
         'Привет! Меня зовут Чекер. \nОтправь мне IMEI для проверки.'
@@ -29,15 +37,8 @@ def send_welcome(message: telebot.types.Message) -> None:
 
 
 @bot.message_handler(commands=['services'])
-def send_service_list(message: telebot.types.Message) -> None:
-    uid = message.from_user.id
-    if not is_user_allowed(uid):
-        bot.send_message(
-            chat_id=message.chat.id,
-            text='У Вас нет доступа к фунционалу этого бота.'
-        )
-        return 
-    
+@access_required
+def send_service_list(message: telebot.types.Message) -> None:  
     response = get_service_list()
     if 'error' in response:
         bot.send_message(
@@ -60,12 +61,8 @@ def send_service_list(message: telebot.types.Message) -> None:
 
 
 @bot.message_handler()
+@access_required
 def send_info_by_imei(message: telebot.types.Message) -> None:
-    uid = message.from_user.id
-    if not is_user_allowed(uid):
-        bot.send_message(message.chat.id, 'У Вас нет доступа к фунционалу этого бота.')
-        return
-
     imei = message.text
     if not is_imei_valid(imei):
         bot.send_message(message.chat.id, 'Некорректный IMEI. Проверьте правильность ввода.')
